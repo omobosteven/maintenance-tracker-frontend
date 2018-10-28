e7caf251f25809c2ea1f1583ffbe08ea6957de38
omobosteven <omobosteven@gmail.com>
2018-10-28 14:34:02 +0100

    ch(setup-react): setup empty react project - install react and react-dom - install and configure webpack and dev-server - install and configure babel - install and configure eslint with airbnb style guide - setup express server for deploying [finishes #161530811]

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * @class App
+ */
+const App = () => (
+  <div>
+    <h1>Welcome to Maintenance Tracker</h1>
+  </div>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[hash].min.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.css', '.scss'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    })
+  ]
+};
