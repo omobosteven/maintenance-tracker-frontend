96f41c0c535e30f0f84f0064d75b9d1e7e7f21fb
omobosteven <omobosteven@gmail.com>
2018-10-31 09:47:54 +0100

    ch(landing-page) change text on landing page [finishes #161614743]

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -13,7 +13,7 @@ export const LandingPage = (props) => {
   return (
     <section className="row main-content">
       <h1 className="main-heading">Maintenance Tracker</h1>
-      <p className="main-text">Lorem Ipsum cool text right here...</p>
+      <p className="main-text">Need Something fixed? Make a request.</p>
       <div className="main-buttons">
         <Link to={routes.SIGN_UP} className="btn main-btn">Signup</Link>
         <Link to={routes.SIGN_IN} className="btn main-btn">Login</Link>
